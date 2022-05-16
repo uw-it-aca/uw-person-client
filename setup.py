50838e088572bb4ffd5e9091d797a793349129ad
Nicholas Falco <ncfalco@uw.edu>
2022-05-16 12:22:47 -0500

    Fix typo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='axdd-person-client',
-    verson=VERSION,
+    version=VERSION,
     packages=['uw_person_client'],
     author="UW-IT AXDD",
     author_email="aca-it@uw.edu",
